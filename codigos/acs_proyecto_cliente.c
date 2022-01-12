60c35d7fa630cd8663dd22436335b7a0b002709b
RAZZ-7 <roy.zacatelco@gmail.com>
2022-01-12 17:43:41 -0600

    Codigo Cliente Terminado

--- codigos/acs_proyecto_cliente.c
+++ codigos/acs_proyecto_cliente.c
@@ -1,7 +1,9 @@
 /*
- **Codigo del Cliente
- **Autores: Martínez Ramírez Pablo César
- **         Zacatelco Zenteno Rodrigo Alberto
+ **UNAM FI
+ **Arquitectura Cliente Servidor 
+ **Grupo:01
+ **Alumnos: Martinez Ramirez Pablo Cesar 
+            Zacatelco Zenteno Rodrigo Alberto
 */
 
 #include <stdio.h>
@@ -15,37 +17,33 @@
 #include <sys/socket.h>
 
 /*
- **MYPORT: Puerto al que conectan los usuarios
- **LINE_MAX: 
- **MAXDATASIZE:
+ **LINE_MAX:    Numero maximo de bytes a escribir
+ **MAXDATASIZE: Numero maximo de bytes a recibir
 */
 
-#define PORT 3490
 #define LINE_MAX 200
-#define MAXDATASIZE 300
+#define MAXDATASIZE 1000
 
 int main(int argc, char *argv[])
 {
   
   /*
    **their_addr: Estructura socket de internet del servidor (informacion sobre su direccion)
-   **sockfd: File descriptor para estructura socket servidor (escuchar sobre sock_fd)
-   **numbytes: Numero real de bytes leidos
-   **buf: BUffer
-   **he: host entry
+   **sockfd:     File descriptor para estructura socket servidor
+   **he:         Estructura que empaqueta toda la informacion sobre un host entrante
+   **port:       Puerto a conectarse
   */
 
-  int sockfd, numbytes;
-  char buf[MAXDATASIZE];
+  int sockfd, port,bEnd=1;
   struct hostent *he;
   struct sockaddr_in their_addr;
 
   /*
-   **Cliente ocupa 2 argumentos: nombre del ejecutable y la IP del servidor. 
-   **Si no son 2 argumentos, se sale de la ejecución
+   **Cliente ocupa 3 argumentos: nombre del ejecutable, IP del servidor y puerto abierto. 
+   **Si no son 3 argumentos, se sale de la ejecución
   */
 
-  if(argc != 2)
+  if(argc != 3)
   {
     fprintf(stderr, "Client-Usage: %s hostname_del_servidor\n", argv[0]);
     exit(1);
@@ -53,10 +51,10 @@ int main(int argc, char *argv[])
 
   /*
    **1. Llamada a funcion gethostbyname para obtener la informacion del host
-   **2. Lo que devuelve la funcion gethostbyname se asigna a la variable he
+   **2. Resultado de gethostbyname se asigna a he
    **3. Compara he contra nulo para validar que no haya errores
    **3.1 Si es igual, llama a funcion perror y se sale de la ejecucion
-   **3.2 Si es diferente, la llamada a la funcion gethostbyname fue exitosa
+   **3.2 Si es diferente, la llamada a gethostbyname fue exitosa
   */
 
   if((he=gethostbyname(argv[1])) == NULL)
@@ -64,46 +62,56 @@ int main(int argc, char *argv[])
     perror("gethostbyname()");
     exit(1);
   }
-  else
-    printf("Client-The remote host is: %s\n", argv[1]);
+  //printf("consola@ACSpy: $Client-The remote host is: %s\n", argv[1]);
 
   /*
-   **1. Llamada a funcion socket
-   **2. Lo que devuelve la funcion socket se asigna a la variable sockfd
-   **3. Compara sockfd contra -1 para validar que no haya errores
+   **1. Llamada a funcion atoi para obtener el puerto en numero entero
+   **2. Resultado de atoio se asigna a port
+   **3. Compara he contra cero para validar que no haya errores
    **3.1 Si es igual, llama a funcion perror y se sale de la ejecucion
-   **3.2 Si es diferente, la llamada a la funcion socket fue exitosa
+   **3.2 Si es diferente, la llamada a atoi fue exitosa
   */
 
- /*
-  Se crea un socket bidieccional, con modo de conexión de flujos de byte,
-  puede proporcionar un mecanismo de transmisión para datos fuera de banda.
+  if((port=atoi(argv[2])) == 0)
+  {
+    perror("consola@ACSpy:Client- Not a valid port number");
+    exit(1);
+  }
+  //printf("consola@ACSpy: $Client-The port number is: %s\n", argv[2]);
+
+  /*
+   **1. Llamada a funcion socket para crear un socket bidieccional, con modo de conexión de flujos de byte
+   **   Puede proporcionar un mecanismo de transmisión para datos fuera de banda.
+   **2. Resultado de socket se asigna a sockfd
+   **3. Compara sockfd contra -1 para validar que no haya errores
+   **3.1 Si es igual, llama a funcion perror y se sale de la ejecucion
+   **3.2 Si es diferente, la llamada a socket fue exitosa
   */
+  
   if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
   {
     perror("socket()");
     exit(1);
   }
-  else
-    printf("Client-The socket() sockfd is OK...\n");
+  //printf("consola@ACSpy: $Client-The socket() sockfd is OK...\n");
 
   /*
    **Inicializar la estructura their_addr para pasarle la IP y el puerto
-   **their_addr.sin_family: Socket TCP/IP (Ordenación de bytes de la máquina) 
-   **their_addr.sin_port: Numero de Puerto (short, Ordenación de bytes de la red)
+   **their_addr.sin_family:      Socket TCP/IP (Ordenación de bytes de la máquina) 
+   **their_addr.sin_port:        Puerto al que se conecta el cliente (short, Ordenación de bytes de la red)
    **their_addr.sin_addr.s_addr: IP en decimal (se usa la primera direccion de la estructura he)
-   **memset: (Poner a cero el resto de la estructura)
+   **Con la funcion memset se pone a cero el resto de la estructura
   */
 
   their_addr.sin_family = AF_INET;
-  printf("Server-Using %s and port %d...\n", argv[1], PORT);
-  their_addr.sin_port = htons(PORT);
+  their_addr.sin_port = htons(port);
   their_addr.sin_addr = *((struct in_addr *)he->h_addr);
   memset(&(their_addr.sin_zero), '\0', 8);
+  //printf("consola@ACSpy: $Client-The server is using %s and port %d...\n", argv[1], argv[2]);
 
   /*
    **1. Llamada a funcion connect
-   **2. Compara lo que devuelve la funcion connect contra -1 para validar que no haya errores
+   **2. Compara el resultado de connect contra -1 para validar que no haya errores
    **2.1 Si es igual, llama a funcion perror y se sale de la ejecucion
    **2.2 Si es diferente, la llamada a la funcion connect fue exitosa
   */
@@ -113,28 +121,55 @@ int main(int argc, char *argv[])
     perror("connect()");
     exit(1);
   }
-  else
-    printf("Client-The connect() is OK...\n");
+  //printf("consola@ACSpy: $Client-The connect() is OK...\n");
   
-  char bTermina[] = "terminar\n";
-  while ( strcmp(bTermina, buf)!=0)
+  /*
+   **Inicia ciclo iterativo una vez se realizo la conexion con el servidor
+   **buf:          Cadena para almacenar la respuesta almacenada
+   **comm:         Cadena para almacenar el comando recibido
+   **numbytes:     Numero maximo de bytes recibidos de respuesta
+   **numbytesComm: Numero maximo de bytes recibidos para el comando
+  */
+
+  while (bEnd!=0)
   {
-    printf("Escribe un mensaje a enviar\n");
-    char linea1[LINE_MAX]; // podemos usarlo por el fgets
-    fgets(linea1,LINE_MAX,stdin);
-    printf("El mensaje a enviar es: %s", linea1);
-    // Envia el mensaje al servidor
-    if (send(sockfd, linea1, strlen(linea1), 0) == -1)
+    char *comm=(char *)malloc(sizeof(char)*LINE_MAX);
+    char buf[MAXDATASIZE];
+    int numbytesComm, numbytes;
+    printf("consola@ACSpy: $");
+    fgets(comm,LINE_MAX,stdin);
+    numbytesComm=strlen(comm);
+    if(numbytesComm<1)
+    {
+      continue;
+    }
+    else if(strcmp("\n", comm)==0)
+    {
+      continue;
+    }
+    else if(strcmp("terminar\n", comm)==0)
+    {
+      if (send(sockfd, comm, numbytesComm, 0) == -1)
+        perror("Server-send() error lol!");
+      bEnd=0;
+      break;
+    }
+    //printf("consola@ACSpy: $Client-The command to send is: %s", comm);
+
+    /*
+     **1.  Llamada a funcion send para escribirle al cliente
+     **2   Compara el resultado de send contra -1 para validar que no haya errores
+     **2.1 Si es igual, llama a funcion perror
+    */
+    if (send(sockfd, comm, numbytesComm, 0) == -1)
       perror("Server-send() error lol!");
 
-    // El cliente ya escribio, ahora va a leer la respuesta del servidor
     /*
     **1. Llamada a funcion recv para recibir mensaje del servidor
-    **   La funcion devuelve el numero real de bytes leidos
-    **2. Lo que devuelve la funcion accept se asigna a la variable new_fd
-    **3. Compara sockfd contra -1 para validar que no haya errores
+    **2. El resultado de recv se asigna a numbytes
+    **3. Compara numbytes contra -1 para validar que no haya errores
     **3.1 Si es igual, llama a funcion perror y se sale de la ejecucion
-    **3.2 Si es diferente, la llamada a la funcion accept fue exitosa
+    **3.2 Si es diferente, la llamada a la funcion recv fue exitosa
     */
 
     if((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1)
@@ -142,15 +177,18 @@ int main(int argc, char *argv[])
       perror("recv()");
       exit(1);
     }
-    else
-      printf("Client-The recv() is OK...\n");
+    //printf("consola@ACSpy: $Client-The recv() is OK...\n");
 
-    //Se trunca el buffer al valor indicado por numbytes
-    buf[numbytes] = '\0';
-    printf("Client-Received: %s", buf);
+    //Si se recibe respuesta, se trunca el buffer al valor indicado por numbytes
+    if (numbytes>0){
+      buf[numbytes] = '\0';
+      //printf("consola@ACSpy: $Client-Received: \n%s", buf);
+      printf("%s", buf);
+    }
+    
   }
   //Cliente cierra su socket y regresa al prompt
-  printf("Client-Closing sockfd (terminar)\n");
+  //printf("consola@ACSpy: $Client-Closing sockfd\n");
   close(sockfd);
   return 0;
 }
